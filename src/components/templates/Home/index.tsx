09b0da827c05c79c17eda9b0284c3e3c6cb046df
akky-xxxx <114.s.aki@gmail.com>
2020-10-31 01:02:45 +0900

    feat: use Spinner

--- src/components/templates/Home/index.tsx
+++ src/components/templates/Home/index.tsx
@@ -2,6 +2,9 @@
 import React, { FC, useEffect } from "react"
 import styled from "styled-components"
 
+// import components
+import { Spinner } from "../../molecules/Spinner"
+
 // import others
 import { HomeProps } from "./types"
 
@@ -13,17 +16,19 @@ export const Home: FC<HomeProps> = (props) => {
     if (!todoItems.data) handleGetTodoItems()
   }, [todoItems.data])
 
-  if (!todoItems.data) return null
-
   return (
     <div>
       <h1>Home</h1>
-      <ul>
-        {todoItems.data.todoItems.map((todoItem) => {
-          const { id, title } = todoItem
-          return <StyledLi key={id}>{title}</StyledLi>
-        })}
-      </ul>
+      <Spinner isShow={todoItems.asyncInfo.isLoading}>
+        {todoItems.data && (
+          <ul>
+            {todoItems.data.todoItems.map((todoItem) => {
+              const { id, title } = todoItem
+              return <StyledLi key={id}>{title}</StyledLi>
+            })}
+          </ul>
+        )}
+      </Spinner>
     </div>
   )
 }
