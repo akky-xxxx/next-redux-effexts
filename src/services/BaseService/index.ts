c8bd774311e614196fe2e29c546986a076efb108
akky-xxxx <114.s.aki@gmail.com>
2020-10-30 21:44:10 +0900

    fix: fix port of api

--- src/services/BaseService/index.ts
+++ src/services/BaseService/index.ts
@@ -4,15 +4,13 @@ import { Service } from "@specter/specter"
 
 // import others
 import { AnyObject } from "../../shared/types/Common"
-import { Server } from "../../shared/const/Server"
 
 // main
-const { PORT } = Server
 export class BaseService extends Service {
   axios: AxiosInstance
 
   constructor(name: string, object: AnyObject) {
     super(name, object)
-    this.axios = axios.create({ baseURL: `http://localhost:${PORT}` })
+    this.axios = axios.create({ baseURL: `http://localhost:3001` })
   }
 }
