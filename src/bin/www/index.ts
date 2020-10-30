74888e65eb014cefcc616ad6cc4f665d40fe54e7
akky-xxxx <114.s.aki@gmail.com>
2020-10-30 20:32:16 +0900

    feat: set logger

--- src/bin/www/index.ts
+++ src/bin/www/index.ts
@@ -5,15 +5,22 @@ import next from "next"
 import { Common } from "../../shared/const/Common"
 import { Server } from "../../shared/const/Server"
 import { initializeExpressServer } from "../../modules/initializeExpressServer"
+import { createLogger } from "../../shared/utils/createLogger"
 
 // main
 const { IS_DEV } = Common
 const { PORT } = Server
+
+const logger = createLogger(__filename)
+logger.debug({ IS_DEV, PORT })
+
 const nextApp = next({
   dev: IS_DEV,
 })
 const expressServer = initializeExpressServer(nextApp)
 
 nextApp.prepare().then(() => {
-  expressServer.listen(PORT)
+  expressServer.listen(PORT, () => {
+    logger.info(`server listening on http://localhost:${PORT}/`)
+  })
 })
--- src/modules/initializeExpressServer/index.ts
+++ src/modules/initializeExpressServer/index.ts
@@ -3,7 +3,11 @@ import express, { Express } from "express"
 import Server from "next/dist/next-server/server/next-server"
 import Routes from "next-routes"
 
+// import others
+import { createLogger } from "../../shared/utils/createLogger"
+
 // main
+const logger = createLogger(__filename)
 const routes = new Routes()
 const expressServer = express()
 
@@ -11,7 +15,9 @@ type InitializeExpressServer = (nextServer: Server) => Express
 export const initializeExpressServer: InitializeExpressServer = (
   nextServer,
 ) => {
+  logger.trace("start")
   const handler = routes.getRequestHandler(nextServer)
   expressServer.use(handler)
+  logger.trace("done")
   return expressServer
 }
