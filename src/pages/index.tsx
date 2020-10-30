26ca261a614de7753a590f913fe8ab12fcb0b187
akky-xxxx <114.s.aki@gmail.com>
2020-10-31 02:46:22 +0900

    feat: update hoc for using bff fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,34 @@
 // import node_modules
 import React from "react"
+import { NextPage } from "next"
 
 // import others
 import { Home } from "../components/templates/Home"
-import { useDispatch, useSelector } from "../store"
+import { wrapper, RootState } from "../store"
 import { readTodoItems } from "../store/reducers/todoItems"
-
-const HomeHoc = () => {
-  const todoItems = useSelector((thisState) => thisState.todoItems)
-  const dispatch = useDispatch()
-  const handleGetTodoItems = () => dispatch(readTodoItems())
-  const props = {
-    todoItems,
-    handleGetTodoItems,
-  }
-
-  return <Home {...props} />
-}
+import { CreateInitialStateAtHoc } from "../shared/types/client/common"
 
 // main
+type HomeHocProps = CreateInitialStateAtHoc<Pick<RootState, "todoItems">>
+
+const HomeHoc: NextPage<HomeHocProps> = (hocProps) => {
+  const {
+    initialState: { todoItems },
+  } = hocProps
+  const pageProps = {
+    todoItems,
+  }
+
+  return <Home {...pageProps} />
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    const {
+      store: { dispatch },
+    } = context
+
+    await dispatch(readTodoItems())
+  },
+)
 export default HomeHoc
