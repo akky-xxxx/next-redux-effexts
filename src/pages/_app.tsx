43b90a787bfb916d7bf1d90c02a8ec1be25d88fa
akky-xxxx <114.s.aki@gmail.com>
2020-11-01 17:59:47 +0900

    feat: fix _app to withRedux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,42 @@
 /**
  * import node_modules
  */
-import App, { AppInitialProps } from "next/app"
+import { AppProps } from "next/app"
 import Head from "next/head"
-import React, { Fragment } from "react"
-import { Provider } from "react-redux"
+import React, { Fragment, FC } from "react"
+import { Provider, useStore } from "react-redux"
 
 /**
  * import others
  */
-import { store } from "../store"
+import { wrapper } from "../store"
 
 /**
  * main
  */
-class MyApp extends App<AppInitialProps> {
-  render() {
-    const { Component, pageProps } = this.props
+const MyApp: FC<AppProps> = (props) => {
+  const { Component, pageProps } = props
+  const store = useStore()
 
-    return (
-      <Fragment>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
-          />
-          <title>next.js</title>
-        </Head>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </Fragment>
-    )
+  const thisPageProps = {
+    ...pageProps,
+    ...store.getState(),
   }
+
+  return (
+    <Fragment>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, minimum-scale=1.0"
+        />
+        <title>next.js</title>
+      </Head>
+      <Provider store={store}>
+        <Component {...thisPageProps} />
+      </Provider>
+    </Fragment>
+  )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp)
