3ef44ee07899eb5b93db57992e9460a26477a2cc
akky-xxxx <114.s.aki@gmail.com>
2020-11-06 22:58:02 +0900

    feat: define common types

--- src/shared/types/Common/index.ts
+++ src/shared/types/Common/index.ts
@@ -1,5 +1,24 @@
 export type AnyObject = Record<string, unknown>
 
+/**
+ * @typedef EmptyObject
+ * @description {} の代替型
+ */
+export type EmptyObject = Record<never, never>
+
 export type ServiceStatusResponse = {
   status?: number
 }
+
+/**
+ * @typedef AnyFunction
+ * @description 仮実装用
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type AnyFunction = (...args: any) => any
+
+/**
+ * @typedef NoArgVoidFunction
+ * @description 引数なし、戻り値なし
+ */
+export type NoArgVoidFunction = () => EmptyObject
