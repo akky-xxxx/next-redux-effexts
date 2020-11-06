9aa012423eb03dfc4712ad75bcf890ad9ac7c529
akky-xxxx <114.s.aki@gmail.com>
2020-11-07 00:54:26 +0900

    feat: define const

---
+++ src/modules/initializeExpressServer/middleware/createErrorHandler/const/index.ts
@@ -0,0 +1,9 @@
+export const HandlerName = {
+  UNEXPECTED: "unexpectedErrorHandler",
+  AXIOS: "axiosErrorHandler",
+}
+
+export const Message = {
+  UN_HANDLING:
+    "unhandled error in bff, you should implements a new ErrorHandlerPair",
+}
