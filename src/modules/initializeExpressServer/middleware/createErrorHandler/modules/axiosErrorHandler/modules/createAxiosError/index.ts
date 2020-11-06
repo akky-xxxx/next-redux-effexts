d7317cfa27dfeceee4dc1616355fd65f690dbe88
akky-xxxx <114.s.aki@gmail.com>
2020-11-07 01:57:43 +0900

    feat: define type of createAxiosError

--- src/modules/initializeExpressServer/middleware/createErrorHandler/modules/axiosErrorHandler/modules/createAxiosError/index.ts
+++ src/modules/initializeExpressServer/middleware/createErrorHandler/modules/axiosErrorHandler/modules/createAxiosError/index.ts
@@ -1,15 +1,19 @@
 // import node_modules
 import { AxiosError } from "axios"
 
+// import others
+import { ThisError } from "../../../../../../../../shared/types/Common"
+
 // main
-export const createAxiosError = (err: AxiosError) => {
+type CreateAxiosError = (err: AxiosError) => ThisError
+export const createAxiosError: CreateAxiosError = (err: AxiosError) => {
   const { message, code, response } = err
 
   return {
     message,
     code,
     status: response?.status || 500,
-    statusText: response?.statusText,
+    statusText: response?.statusText || "Internal Server Error.",
     request: {
       headers: response?.headers,
     },
