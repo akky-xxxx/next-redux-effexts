e2951e867291daf03243c1a9ad2f34c1a8c15863
akky-xxxx <114.s.aki@gmail.com>
2020-11-07 17:27:34 +0900

    style: apply fix task

--- src/components/templates/Home/modules/useHome/index.ts
+++ src/components/templates/Home/modules/useHome/index.ts
@@ -4,8 +4,10 @@ import { HomeProps } from "../../types"
 // main
 export const useHome = (props: HomeProps) => {
   const { todoItems, todoItemsId } = props
-  const errorMessage = todoItems.asyncInfo.errorMessage || todoItemsId.asyncInfo.errorMessage
-  const isShowSpinner = todoItems.asyncInfo.isLoading || todoItemsId.asyncInfo.isLoading
+  const errorMessage =
+    todoItems.asyncInfo.errorMessage || todoItemsId.asyncInfo.errorMessage
+  const isShowSpinner =
+    todoItems.asyncInfo.isLoading || todoItemsId.asyncInfo.isLoading
 
   return { errorMessage, isShowSpinner }
 }
