75e292a475102418f7157feaaf8cbaad503d229e
akky-xxxx <114.s.aki@gmail.com>
2020-11-07 13:35:42 +0900

    feat: define List page

---
+++ src/components/templates/List/index.tsx
@@ -0,0 +1,15 @@
+// import node_modules
+import React, { FC } from "react"
+
+// import components
+import { Navigation } from "../../molecules/Navigation"
+
+// main
+export const List: FC = () => {
+  return (
+    <div>
+      <h1>List</h1>
+      <Navigation />
+    </div>
+  )
+}
---
+++ src/pages/list/index.tsx
@@ -0,0 +1,13 @@
+// import node_modules
+import React from "react"
+import { NextPage } from "next"
+
+// import components
+import { List } from "../../components/templates/List"
+
+// main
+const ListHoc: NextPage = () => {
+  return <List />
+}
+
+export default ListHoc
